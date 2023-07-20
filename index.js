70c4d687b1d9ceb6cf528ea99745cbe9fbbdd296
abhaykanaujiya <abhaykanaujiya@gmail.com>
2023-07-21 03:13:27 +0530

    node setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mangoose = require('mongoose')
 const bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
@@ -7,14 +8,15 @@ const app = express();
 mangoose.connect(url, { useNewUrlParser: true })
 const connection = mongoose.connection
 
-connection.on('open', function () {
-  console.log('contected....');
-})
+// connection.on('open', function () {
+//   console.log('contected....');
+// })
 
-const alienRouter = require('./routes/aliens')
-app.use('/aliens',alienRouter)
-// const state = [];
-// app.use(bodyParser.json());
+// const alienRouter = require('./routes/aliens')
+// app.use('/aliens',alienRouter)
+const state = [];
+app.use(bodyParser.json());
+app.use(cors())
 // app.get('/user', (req, res) => {
 //   return res.status(200).json({ data: state })
 // })
@@ -24,11 +26,14 @@ app.use('/aliens',alienRouter)
 //   console.log(id);
 //   return res.status(200).json({msg:id})
 // })
-// app.post('/form_data', (req, res) => {
-//   const { name,email,password } = req.body;
-//   state.push({ name,email,password})
-//   return res.status(200).json({ msg: "success" })
-// })
+app.post('/form_data', (req, res) => {
+
+  const { name, email, phone, address } = req.body.data;
+  console.log(name,email,address,req.body);
+  state.push({ name, email, phone, address })
+    console.log(state);
+   return res.send('User has been added successfully');
+})
 
 app.listen(2020, () => {
   console.log('server is up')
